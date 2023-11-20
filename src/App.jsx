51a35050fd610cc5c3718ae16ebff790202c46a8
aajy <aajy.000@gmail.com>
2023-11-20 11:45:28 +0900

    snippet으로 컴포넌트 생성, 불러오기

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Btn from './Btn';
+import Title from './Title';
+import Sub from './sub/Sub';
+
+function App() {
+	return (
+		<div className='App'>
+			<Title />
+			<Btn />
+			<Sub />
+		</div>
+	);
+}
+
+export default App;

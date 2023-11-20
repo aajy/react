82a45b62bf9fb4fb0c2ad0c3ecf66db5a7a24268
aajy <aajy.000@gmail.com>
2023-11-20 23:47:19 +0900

    컴포넌트에 이벤트연결 화살표함수로.

--- src/components/sub/News.jsx
+++ src/components/sub/News.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 export default function News() {
+	const handleClick = (e) => {
+		console.log(e);
+		console.log(e.currentTarget.innerText);
+	};
 	return (
 		<article>
 			<h2>Title comes here.</h2>
 			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, enim?</p>
-			<a href='#'>View Detail</a>
+			<a href='#' onClick={(e) => handleClick(e)}>
+				View Detail
+			</a>
 		</article>
 	);
 }

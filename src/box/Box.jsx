31eec842c9c12e8362de9896b584fd065aa24b3b
aajy <aajy.000@gmail.com>
2023-11-21 11:19:52 +0900

    배경바꾸는 이벤트핸들러1

--- src/box/Box.jsx
+++ src/box/Box.jsx
@@ -1,4 +1,11 @@
 import './Box.scss';
 export default function Box() {
-	return <div className='Box '>Box</div>;
+	const changeBGColor = (e, color) => {
+		e.target.style.backgroundColor = color;
+	};
+	return (
+		<div className='Box ' onClick={(e) => changeBGColor(e, 'hotpink')}>
+			Box
+		</div>
+	);
 }

d84eda011b22286388466d9f742f6978eccb43bf
aajy <aajy.000@gmail.com>
2023-11-21 23:15:29 +0900

    Card컴포넌트 배열로 생성, props값 비구조화할당으로 전달, defalt값 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
+import './scss/style.scss';
 import Card from './card/Card';
+import pic1 from './img/pic1.png';
+import pic2 from './img/pic2.png';
+import pic3 from './img/pic3.png';
 
 export default function App() {
-	const colors = ['orange', 'hotpink', 'aqua', 'violet', 'green'];
-	const width = [100, 200, 300, 400, 500];
+	const info = [
+		{
+			img: pic1,
+			title: 'Healthcare',
+		},
+		{
+			img: pic2,
+			title: 'Oil and Gas',
+		},
+		{
+			img: pic3,
+			title: 'AgriBusiness',
+		},
+	];
 	return (
 		<>
-			<h1>Color Chart</h1>
-			{colors.map((color, idx) => {
-				return (
-					<Card
-						key={color + idx}
-						colorName={color}
-						index={idx}
-						width={width[idx]}
-						istxtVisible={width[idx] > 200 ? true : false}
-					/>
-				);
-			})}
-			<Card />
+			<div className='wrap'>
+				<div className='top'>
+					<h1>Our Industry specializtion</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sint fuga suscipit ab
+						repudiandae rerum totam ut blanditiis? Quos, adipisci.
+					</p>
+				</div>
+				<div className='CardBox'>
+					{info.map((obj, index) => {
+						return <Card img={obj.img} title={obj.title} index={index} key={obj.title + index} />;
+					})}
+				</div>
+			</div>
 		</>
 	);
 }
--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,16 +1,12 @@
 import './Card.scss';
-export default function Card({ colorName = '#666', index = 0, width = 100, istxtVisible = true }) {
-	const style = {
-		width: width,
-	};
+export default function Card({ img, title = 'Title$', index = '$' }) {
 	return (
-		<article className='Card ' style={style}>
-			<div className='bgBox' style={{ backgroundColor: colorName }}></div>
-			{istxtVisible && (
-				<div className='txtBox'>
-					{index + 1}-{colorName}
-				</div>
-			)}
+		<article className='Card '>
+			<img src='' alt='' />
+			<div className='textBox'>
+				<span>0{index + 1}</span>
+				<p>{title}</p>
+			</div>
 		</article>
 	);
 }

7e1a8be4d9c325ca436a6ccd5fa26fc5437fecfa
aajy <aajy.000@gmail.com>
2023-11-22 17:19:33 +0900

    state사용시 후위증감연산자x, state값을 연산한 값은 o

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import { useState } from 'react';
-import Modal from './modal/Modal';
-
+import './App.scss';
 export default function App() {
-	const [IsOpen, setIsOpen] = useState(false);
-	const [Color, setColor] = useState('black');
-	const [DBColors, setDBColors] = useState(['hotpink', 'red', 'aqua', 'orange']);
-	const changeColor = (color) => {
-		setColor(color);
-	};
+	console.log('render');
+	const [Index, setIndex] = useState(0);
+
 	return (
 		<>
-			{DBColors.map((color, index) => {
-				return (
-					<button onClick={() => changeColor(color)} key={color + index}>
-						{color}
-					</button>
-				);
-			})}
-			<h1 style={{ color: Color }}>부모 컴포넌트</h1>
-			<button onClick={() => setIsOpen(!IsOpen)}>{IsOpen ? '모달 닫기' : '모달 열기'}</button>
-			{IsOpen && <Modal color={Color} />}
+			<button onClick={() => setIndex(Index - 1)}>left</button>
+			<button onClick={() => setIndex(Index + 1)}>right</button>
+			<div className='box' style={{ transform: `rotate(${45 * Index}deg)` }}></div>
 		</>
 	);
 }
--- src/modal/Modal.jsx
+++
@@ -1,8 +0,0 @@
-import './Modal.scss';
-export default function Modal({ color }) {
-	return (
-		<div className='Modal' style={{ backgroundColor: color }}>
-			Modal
-		</div>
-	);
-}

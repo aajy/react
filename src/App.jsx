9979dd36392a11e5dad0e92efaf83b6717d3ad49
aajy <aajy.000@gmail.com>
2023-11-22 21:48:02 +0900

    useRef를 이용해서 참조객체 생성하여 정보값사용해서 버튼으로 원하는 rotate주기

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.scss';
 export default function App() {
 	console.log('render');
+	const box = useRef(null);
+	const deg = useRef(45);
 	const [Index, setIndex] = useState(0);
-
+	const handleLeft = () => {
+		setIndex(Index - 1);
+		box.current.style.transform = `rotate(${deg.current * Index}deg)`;
+	};
+	const handleRight = () => {
+		setIndex(Index + 1);
+		box.current.style.transform = `rotate(${deg.current * Index}deg)`;
+	};
 	return (
 		<>
-			<button onClick={() => setIndex(Index - 1)}>left</button>
-			<button onClick={() => setIndex(Index + 1)}>right</button>
-			<div className='box' style={{ transform: `rotate(${45 * Index}deg)` }}></div>
+			<div className='wrap'>
+				<button onClick={handleLeft}>left</button>
+				<button onClick={handleRight}>right</button>
+				<div className='box' ref={box}></div>
+			</div>
 		</>
 	);
 }

ca1c7051db7bffa5829822e09e5cb2797fb69ca7
aajy <aajy.000@gmail.com>
2023-11-22 10:50:51 +0900

    useState를 쓰지 않고 모달을 열기위한 버튼을 만든다면.

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,17 @@
-import './scss/style.scss';
-import Card from './card/Card';
-import pic1 from './img/pic1.png';
-import pic2 from './img/pic2.png';
-import pic3 from './img/pic3.png';
+import Modal from './modal/Modal';
 
 export default function App() {
-	const info = [
-		{
-			img: pic1,
-			title: 'Healthcare',
-		},
-		{
-			img: pic2,
-			title: 'Oil and Gas',
-		},
-		{
-			img: pic3,
-			title: 'AgriBusiness',
-		},
-	];
+	console.log('App re-rendered');
+	let IsOpen = false;
+	const handleModal = () => {
+		console.log('handlemodal called');
+		IsOpen = true;
+	};
 	return (
 		<>
-			<div className='wrap'>
-				<div className='top'>
-					<h1>Our Industry specializtion</h1>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sint fuga suscipit ab
-						repudiandae rerum totam ut blanditiis? Quos, adipisci.
-					</p>
-				</div>
-				<div className='CardBox'>
-					{info.map((obj, index) => {
-						return <Card img={obj.img} title={obj.title} index={index} key={obj.title + index} />;
-					})}
-				</div>
-			</div>
+			<h1>부모 컴포넌트</h1>
+			<button onClick={handleModal}>modal {IsOpen ? 'close' : 'open'}</button>
+			{IsOpen && <Modal />}
 		</>
 	);
 }
---
+++ src/modal/Modal.jsx
@@ -0,0 +1,4 @@
+import './Modal.scss';
+export default function Modal() {
+	return <div className='Modal'>Modal</div>;
+}

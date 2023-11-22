d6ba623616980dbb978761f7c8b7aa7ba6321666
aajy <aajy.000@gmail.com>
2023-11-22 11:48:34 +0900

    state로 글자색 변경

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react';
 import Modal from './modal/Modal';
 
 export default function App() {
-	const [IsOpen, setIsOpen] = useState();
+	const [IsOpen, setIsOpen] = useState(false);
+	const [Color, setColor] = useState('black');
 
+	const changeColor = (color) => {
+		setColor(color);
+	};
 	return (
 		<>
-			<h1>부모 컴포넌트</h1>
+			<button
+				onClick={() => {
+					changeColor('green');
+				}}
+			>
+				green
+			</button>
+			<h1 style={{ color: Color }}>부모 컴포넌트</h1>
 			<button onClick={() => setIsOpen(!IsOpen)}>{IsOpen ? '모달 닫기' : '모달 열기'}</button>
 			{IsOpen && <Modal />}
 		</>

46b2ed2473a5b0a63dd9a79c6434158bc59b6d0d
aajy <aajy.000@gmail.com>
2023-11-22 23:09:51 +0900

    useEffect의존성배열빈배열과 useRef로 focus주기

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.scss';
+import { useRef } from 'react';
 export default function App() {
+	const inputRef = useRef(null);
 	const [DataObj, setDataObj] = useState({
 		first: '',
 		second: '',
@@ -11,10 +13,13 @@ export default function App() {
 	useEffect(() => {
 		console.log(DataObj);
 	}, [DataObj]);
+	useEffect(() => {
+		inputRef.current.focus();
+	}, []);
 	return (
 		<>
 			<div className='wrap'>
-				<input type='text' name='first' value={DataObj.first} onChange={changeInput} />
+				<input type='text' name='first' value={DataObj.first} onChange={changeInput} ref={inputRef} />
 				<input type='text' name='second' value={DataObj.second} onChange={changeInput} />
 			</div>
 		</>

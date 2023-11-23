34d61ae4e0cf5a94c4877055f5d271de2328198c
aajy <aajy.000@gmail.com>
2023-11-23 23:48:32 +0900

    useRef 사용예시1. 마운트 시 input focus 주기

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import './App.scss';
 export default function App() {
 	console.log('render');
-	const [Num2, setNum2] = useState(0);
-	let num1 = useRef(0);
-	const handleClick = () => {
-		num1.current++;
-		setNum2(Num2 + 1);
-		console.log('num1', num1.current);
-		console.log('num2', Num2);
-	};
+	let inputRef = useRef(0);
+
+	useEffect(() => {
+		inputRef.current.focus();
+	}, []);
 	return (
 		<>
-			<button onClick={handleClick}>버튼</button>
+			<input type='text' ref={inputRef} />
 		</>
 	);
 }

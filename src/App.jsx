2891d73266b179732ad69fe421ae0cbe0955fd91
aajy <aajy.000@gmail.com>
2023-11-23 23:50:50 +0900

    useRef 예시 2. state가set함수에 의해 바뀌어서 랜더링 되기 전에 현재 state값을 저장하는용도

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 export default function App() {
-	console.log('render');
-	let inputRef = useRef(0);
+	const [count, setCount] = useState(0);
+	const prevCountRef = useRef();
 
 	useEffect(() => {
-		inputRef.current.focus();
-	}, []);
+		// 컴포넌트가 업데이트될 때마다 현재 상태를 prevCountRef에 저장
+		prevCountRef.current = count;
+	});
+
+	const handleIncrement = () => {
+		setCount(count + 1);
+	};
 	return (
-		<>
-			<input type='text' ref={inputRef} />
-		</>
+		<div>
+			<p>Current Count: {count}</p>
+			<p>Previous Count: {prevCountRef.current}</p>
+			<button onClick={handleIncrement}>Increment</button>
+		</div>
 	);
 }

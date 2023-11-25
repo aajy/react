d1b13d8312ee45dd368244972fc3b18ea29d94b7
aajy <aajy.000@gmail.com>
2023-11-25 23:08:02 +0900

    useRef으로  DOM참조객체에 담는 법, 모션작업시 수치값 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import './App.scss';
 export default function App() {
-	const [count, setCount] = useState(0);
-	const prevCountRef = useRef();
-
-	useEffect(() => {
-		// 컴포넌트가 업데이트될 때마다 현재 상태를 prevCountRef에 저장
-		prevCountRef.current = count;
-	});
-
-	const handleIncrement = () => {
-		setCount(count + 1);
-	};
+	const box = useRef(null);
+	const count = useRef(0);
+	const boxStyle= {
+		border: '1px solid #333',
+		width:300,
+		height:300,
+		transition:'0.5s',
+		marginTop:100
+	}
+	const plus = () => {
+		count.current = count.current + 1;
+		box.current.style.transform = `rotate(${count.current*45}deg)`;
+	}
+	const minus = () => {
+		count.current = count.current - 1;
+		box.current.style.transform = `rotate(${count.current*45}deg)`;
+	}
 	return (
-		<div>
-			<p>Current Count: {count}</p>
-			<p>Previous Count: {prevCountRef.current}</p>
-			<button onClick={handleIncrement}>Increment</button>
+		<div className="wrap">
+			<button onClick={()=>plus()}>left</button>
+			<button onClick={()=>minus()}>right</button>
+			
+			<article style={boxStyle} ref={box}></article>
 		</div>
 	);
 }

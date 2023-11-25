99100bfe656e8d6903814b58e1e4a142521768b7
aajy <aajy.000@gmail.com>
2023-11-25 23:43:51 +0900

    filter버튼 생성, css추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ import { useEffect,useState } from 'react';
 export default function App() {
 	const [Members,setMembers] = useState([]);
 	const colors =['red','orangered', 'orange','lightgoldenrodyellow','yellowgreen','cornflowerblue']
+
+	const filter = (e) => {
+		console.log(e.target.innerText);
+	}
 useEffect(()=>{
 	setMembers(data.members);
 	console.log(data);
 },[]);
 	return (
 		<div className="wrap">
+			<div className='filter'>
+				<button onClick={(e)=>filter(e)}>David</button>
+				<button onClick={(e)=>filter(e)}>Emily</button>
+				<button onClick={(e)=>filter(e)}>Emma</button>
+			</div>
 			{ Members.map((el, index)=>{
 				return <div className='box' style={{borderColor:`${colors[index]}`, color:`${colors[index]}`,}}>
 				<span style={{backgroundColor:`${colors[index]}`, borderColor:`${colors[index]}`}}></span>

06ef6c9a2070cc3628f09a5e643b15b7064aaad9
aajy <aajy.000@gmail.com>
2023-11-26 23:21:47 +0900

    useContext 사용해보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,67 @@
 import './App.scss';
 import data from '../src/DB/department.json';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
+import { NameContext } from './components/common/NameContext';
+import { AgeContext } from './components/common/AgeContext';
+import Header from './components/common/Header';
 
 export default function App() {
-	const [Members,setMembers] = useState([]);
-	const colors =['red','orangered', 'orange','lightgoldenrodyellow','yellowgreen','cornflowerblue']
+	const [Members, setMembers] = useState([]);
+	const colors = [
+		'red',
+		'orangered',
+		'orange',
+		'lightgoldenrodyellow',
+		'yellowgreen',
+		'cornflowerblue',
+	];
 
 	const filter = (e) => {
 		const arr = data.members;
 		const name = e.target.innerText;
-		const member = arr.filter(el=>el.name===name);
+		const member = arr.filter((el) => el.name === name);
 		console.log('member: ', member);
 		setMembers(member);
-	}
-useEffect(()=>{
-	setMembers(data.members);
-	console.log(data);
-},[]);
+	};
+	useEffect(() => {
+		setMembers(data.members);
+		console.log(data);
+	}, []);
 	return (
-		<div className="wrap">
+		<div className='wrap'>
+			<AgeContext.Provider value='20'>
+				<NameContext.Provider value='홍길동'>
+					<Header />
+				</NameContext.Provider>
+			</AgeContext.Provider>
 			<span>{Members.length}</span>
 			<div className='filter'>
-				<button onClick={(e)=>filter(e)}>David</button>
-				<button onClick={(e)=>filter(e)}>Emily</button>
-				<button onClick={(e)=>filter(e)}>Emma</button>
+				<button onClick={(e) => filter(e)}>David</button>
+				<button onClick={(e) => filter(e)}>Emily</button>
+				<button onClick={(e) => filter(e)}>Emma</button>
 			</div>
-			{ Members.map((el, index)=>{
-				return <div className='box' style={{borderColor:`${colors[index]}`, color:`${colors[index]}`,}}>
-				<span style={{backgroundColor:`${colors[index]}`, borderColor:`${colors[index]}`}}></span>
-				<div className='boxRight'>
-					<span style={{borderColor:`${colors[index]}`}}>{el.name}</span>
-					<span>{el.position}</span>
-				</div>
-			</div>
-})}
+			{Members.map((el, index) => {
+				return (
+					<div
+						className='box'
+						style={{
+							borderColor: `${colors[index]}`,
+							color: `${colors[index]}`,
+						}}
+					>
+						<span
+							style={{
+								backgroundColor: `${colors[index]}`,
+								borderColor: `${colors[index]}`,
+							}}
+						></span>
+						<div className='boxRight'>
+							<span style={{ borderColor: `${colors[index]}` }}>{el.name}</span>
+							<span>{el.position}</span>
+						</div>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,3 +1,15 @@
+import { useContext } from 'react';
+import { AgeContext } from './AgeContext';
+import { NameContext } from './NameContext';
+
 export default function Header() {
-	return <header>Header</header>;
+	const age = useContext(AgeContext);
+	const user = useContext(NameContext);
+	return (
+		<header>
+			<p>
+				환영합니다. {user}님의 나이는 {age}입니다.
+			</p>
+		</header>
+	);
 }

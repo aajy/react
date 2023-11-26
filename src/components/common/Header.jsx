516941ad3478053a192d06e280097df7eda7919a
aajy <aajy.000@gmail.com>
2023-11-26 23:29:24 +0900

    usMemo사용해보기, useRef와 비슷하게? 다시 변수 메모리에 새로 할당되는 것이 아니라, 메모리에 저장되어있는 값을 다시 재사용할수있도록.

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,15 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useMemo, useEffect, useState } from 'react';
 import { AgeContext } from './AgeContext';
 import { NameContext } from './NameContext';
 
 export default function Header() {
 	const age = useContext(AgeContext);
 	const user = useContext(NameContext);
+
+	const [number, setNumber] = useState(0);
+	const [isKorea, setIsKorea] = useState(true);
+	// const location = { country: isKorea ? '한국' : '일본' };
+	const location = useMemo(() => {
+		return {
+			country: isKorea ? '한국' : '일본',
+		};
+	}, [isKorea]);
+
+	useEffect(() => {
+		console.log('useEffect... 호출');
+		// 뭔가 오래 걸리는 작업
+	}, [location]);
 	return (
 		<header>
 			<p>
 				환영합니다. {user}님의 나이는 {age}입니다.
 			</p>
+			<h2>어떤 수가 좋은가요?</h2>
+			<input
+				type='number'
+				value={number}
+				onChange={(e) => setNumber(e.target.value)}
+			/>
+			<hr />
+			<h2>어느 나라에 있어요?</h2>
+			<p>나라: {location.country}</p>
+			<button onClick={() => setIsKorea(!isKorea)}>Update</button>
 		</header>
 	);
 }
